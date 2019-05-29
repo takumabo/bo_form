30f38ee8b14662bfd651f7082955429a258cc339
takumabo <kingcongyatuhashi.1043@gmail.com>
2019-05-29 15:49:15 +0800

    お問い合わせ確認画面とお問い合わせ完了を追加
    値を隠して画面を加えたいから

---
+++ thanks.php
@@ -0,0 +1,19 @@
+<?php
+    $nickname = $_POST['nickname'];
+    $email = $_POST['email'];
+    $content = $_POST['content'];
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>送信完了</title>
+</head>
+<body>
+    <h1>お問い合わせありがとうございました。</h1>
+    <p><?php echo $nickname ?></p>
+    <p><?php echo $email ?></p>
+    <p><?php echo $content ?></p>
+</body>
+</html>
